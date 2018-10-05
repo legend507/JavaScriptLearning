fa8f1f42184cd8133e417a910a52f7b605c5ab69
legend507 <chenfeiyu507@gmail.com>
2018-10-05 15:11:00 +0900

    finished Pig Game, now it is fully playable

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,26 +9,90 @@ GAME RULES:
 
 */
 
-/*
-// Math.random() yields a float valune between [0,1]
-console.log(Math.random())
-
-// to make the random valune between [1,6], do
-console.log(Math.floor( Math.random() * 6 ) + 1)
-*/
-
 var activePlayer = 0;   // 0 = player1, 1 = player2
+var roundScore = 0;
+var scores = [0,0];
 
-// to generate a var with a random dice value
-var dice = Math.floor( Math.random() * 6 ) + 1;
+reset();
 
-// to modify DOM using document object, 1. use querySelector to choose the content, 2. use textContent to change the content
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// or 2. use innerHTML to parse a line of HTML code 
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// when "roll" button is clicked
+//  the following line adds event listener to the "roll" button
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. generate a random number [1,6]
+    var dice = Math.floor( Math.random() * 6 ) + 1;
 
-// to get current HTML content
-var content = document.querySelector('#current-0').textContent;
+    // 2. display the corresponding dice img (querySelector interact with .css file)
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-// to hide the dice.img at first, note .dice is the class name
-document.querySelector('.dice').style.display = 'none';
+    // 3. update the "round score", if the dice != 1
+    if ( dice == 1 ) {
+        // when dice result is 1,
+        switchActivePlayer(false);
+    } else {
+        roundScore += dice;
+
+        // to dispplay the current roundScore
+        //  method 1 (getElementById interact with HTML, faster than querySelector)
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        //  method 2 (innerHTML)
+        //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+    }
+});
+
+// when "hold" button pressed
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    switchActivePlayer(true);
+});
+
+document.querySelector('.btn-new').addEventListener('dblclick', reset);
+
+function reset() {
+    // to hide the dice.img at first, note .dice is the class name
+    //  the following line modifies the property of "<img src="dice-5.png" alt="Dice" class="dice">" in HTML
+    document.querySelector('.dice').style.display = 'none';
+
+    // initialize the socres of Player 1 & 2
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    activePlayer = 0;
+    roundScore = 0;
+    scores = [0,0];
+}
+
+function switchActivePlayer(isHold) {
+    // is 'Hold' is pressed, add current round score to score
+    if (isHold) {
+        scores[activePlayer] += roundScore;
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+        // check if score >= 100
+        if (scores[activePlayer] >= 100) {
+            //alert('Player ' + (activePlayer + 1) + ' is the Winner!');
+
+            // apply 'winner' class to .player-?-panel 
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            
+            reset();
+            return;
+        }
+    }
+
+    //  1. clear current player's roundScore
+    roundScore = 0;
+    //  2. change player GUI to 0
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+    //  3. remove the active marker on GUI
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    //  4. switch active player
+    activePlayer = 1 - activePlayer;
+    //  5. add the active marker on GUI
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+
+    /* instead of using 'remove' & 'add', use 'toggle' for simplicity */
+}
